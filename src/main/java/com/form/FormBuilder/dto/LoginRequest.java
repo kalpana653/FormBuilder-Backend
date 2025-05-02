9fb94423a64e6956155f49c0621a44c30e65dbb3
Kalpana653 <kalpanavelu2003@gmail.com>
2025-05-02 10:51:52 +0530

    .\.git\

--- src/main/java/com/form/FormBuilder/dto/LoginRequest.java
+++ src/main/java/com/form/FormBuilder/dto/LoginRequest.java
@@ -1,15 +1,23 @@
 package com.form.FormBuilder.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class LoginRequest {
     
     @NotBlank(message = "Email is required")
     @Email(message = "Please provide a valid email address")
     private String email;
+    
+    // Added username field to support login with username
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    private String password;
 
     public @NotBlank(message = "Email is required") @Email(message = "Please provide a valid email address") String getEmail() {
         return email;
@@ -17,6 +25,14 @@ public class LoginRequest {
 
     public void setEmail(@NotBlank(message = "Email is required") @Email(message = "Please provide a valid email address") String email) {
         this.email = email;
+    }
+    
+    public String getUsername() {
+        return username;
+    }
+    
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public @NotBlank(message = "Password is required") String getPassword() {
@@ -26,8 +42,4 @@ public class LoginRequest {
     public void setPassword(@NotBlank(message = "Password is required") String password) {
         this.password = password;
     }
-
-    @NotBlank(message = "Password is required")
-    private String password;
-
 }
